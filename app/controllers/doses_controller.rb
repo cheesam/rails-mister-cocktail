5f114ce6163d10a137b709497049461b940cf584
Sam Cheesman <s.cheesman1234500@gmail.com>
2019-07-29 08:38:19 +0100

    Working on the association of the doses to the cocktails

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,17 +1,37 @@
 class DosesController < ApplicationController
-
-  def create
-    @dose = Dose.create(dose_params)
-    redirect_to cocktails_path
-  end
+  before_action :set_dose, only: [:destroy]
+  before_action :set_cocktail, only: [:new, :create]
 
   def new
     @dose = Dose.new
   end
 
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
   private
 
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
   def dose_params
-    params.require(:dose).permit(:name)
+    params.require(:dose).permit(:cocktail_id, :description, :ingredient_id)
   end
 end
